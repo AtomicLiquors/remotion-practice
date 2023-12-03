a111a71c19959d7602861871ae1cb03dcd354f15
chb <gyqls234@gmail.com>
2023-12-03 21:15:48 +0900

    21:15:

---
+++ remotion-stories/src/Message.tsx
@@ -0,0 +1,10 @@
+import { SingleMessageApiResponse } from "./types"
+
+
+export const Message: React.FC<{
+  data: SingleMessageApiResponse
+}> = ({data}) => {
+  return (
+    <div>items</div>
+  )
+}
--- remotion-stories/src/Stories.tsx
+++ remotion-stories/src/Stories.tsx
@@ -1,28 +1,48 @@
-import {AbsoluteFill} from 'remotion';
+import {AbsoluteFill, continueRender, delayRender} from 'remotion';
 import {Background} from './Background';
-import {useCallback, useEffect} from 'react';
+import {useCallback, useEffect, useState} from 'react';
+import { SingleMessageApiResponse } from './types';
 
 export const Stories: React.FC<{
 	messageIds: string[];
 }> = ({messageIds}) => {
+  const [handle] = useState(() => delayRender());
+
+  const [messages, setMessages] = useState<SingleMessageApiResponse[] | null>(
+    null
+  );
 	const fetchMessages = useCallback(async () => {
-		const messages = await Promise.all(
+		const fetchedMessages = await Promise.all(
 			messageIds.map(async (m) => {
 				const response = await fetch(`https://cat-fact.herokuapp.com/facts`);
 				const json = await response.json();
 				return json;
 			})
 		);
-    console.log(messages);
+    setMessages(fetchedMessages);
+//    
+    continueRender(handle);
 	}, [messageIds]);
 
   useEffect(() => {
     fetchMessages();
   }, [fetchMessages])
 
+  if (!messages) {
+    return null;
+  }
+
+  console.log({messages});
+
 	return (
 		<AbsoluteFill>
 			<Background />
+
+      {messages.map((m) => {
+        return (
+          <div style={{fontSize: 80}}>items</div>
+        )
+      })}
 		</AbsoluteFill>
 	);
 };
